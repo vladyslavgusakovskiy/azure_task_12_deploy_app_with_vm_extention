e85330905634fa484dbf1c8e4fd8f8dbee8e7f51
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-05-15 13:37:10 +0300

    update the link with correct GitHub username

--- install-app.sh
+++ install-app.sh
@@ -13,7 +13,7 @@ apt-get install python3-pip -yq
 # Create a directory for the app and download the files. 
 mkdir /app 
 # make sure to uncomment the line bellow and update the link with your GitHub username
-# git clone https://github.com/<your-gh-username>/azure_task_12_deploy_app_with_vm_extention.git
+git clone https://github.com/vladyslavgusakovskiy/azure_task_12_deploy_app_with_vm_extention.git
 cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
 
 # create a service for the app via systemctl and start the app
